e3e73288bff886ee8dec807906d6b89f02b6172d
PARK GI-PYO <gipyopark@gmail.com>
2024-04-15 15:34:11 +0900

    [init] #1 - spring boot 프로젝트 구성
    1. `gitignore` 작성
    2.  `build.gradle` - web, lombok 의존성 추가
    
    기본적인 spring boot 프로젝트만 구성했다.
    그 외에 필요한 것들은 그때마다 추가해서 사용하도록 한다.

---
+++ lottery-purchaser/src/test/java/com/lottog/purchaser/LotteryPurchaserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottog.purchaser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryPurchaserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
