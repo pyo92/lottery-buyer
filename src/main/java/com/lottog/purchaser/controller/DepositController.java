0cf7c1d1404948c65b0f7959ccc0342a65882a62
PARK GI-PYO <gipyopark@gmail.com>
2024-04-18 21:53:21 +0900

    [feat] #9 - 예치금 입금 신청 controller - POST method 추가
    예치금 입금 신청 처리 `POST` method 를 작성했다.

--- src/main/java/com/lottog/purchaser/controller/DepositController.java
+++ src/main/java/com/lottog/purchaser/controller/DepositController.java
@@ -1,16 +1,15 @@
 package com.lottog.purchaser.controller;
 
+import com.lottog.purchaser.dto.request.PaymentRequest;
 import com.lottog.purchaser.dto.response.ErrorResponse;
 import com.lottog.purchaser.dto.response.LoginResponse;
-import com.lottog.purchaser.service.DepositService;
 import com.lottog.purchaser.service.LoginService;
+import com.lottog.purchaser.service.DepositService;
 import com.lottog.purchaser.service.SeleniumService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -44,6 +43,33 @@ public class DepositController {
         } catch (Exception e) {
             log.error("=== getDeposit() occurred error - {}", e.getMessage());
             ErrorResponse response = ErrorResponse.status500("getDeposit() - 오류가 발생했습니다. (id = " + id + ") - " + e.getMessage());
+
+            return ResponseEntity
+                    .status(response.status())
+                    .body(response);
+
+        } finally {
+            seleniumService.closeWebDriver();
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("/payment")
+    public ResponseEntity<?> payment(@RequestBody PaymentRequest request) {
+        try {
+            //로그인 처리
+            LoginResponse loginResponse = loginService.login(request.id());
+
+            //로그인 실패 시, 결과 반환 처리
+            if (!loginResponse.success()) {
+                return ResponseEntity.ok(loginResponse);
+            }
+
+            return ResponseEntity.ok(depositService.payment());
+
+        } catch (Exception e) {
+            log.error("=== putDeposit() occurred error - {}", e.getMessage());
+            ErrorResponse response = ErrorResponse.status500("getDeposit() - 오류가 발생했습니다. (id = " + request.id() + ") - " + e.getMessage());
 
             return ResponseEntity
                     .status(response.status())
